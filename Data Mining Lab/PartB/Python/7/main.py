065225a63c1d0fb9bef1e9c5ba2e5fcb37d19405
Tanisha <tanisha031199@gmail.com>
2021-02-15 14:31:50 +0530

    changes to program

--- Data Mining Lab/PartB/Python/7/main.py
+++ Data Mining Lab/PartB/Python/7/main.py
@@ -13,39 +13,38 @@ print(df.isnull().sum())
 df.gender.fillna(df.gender.mode()[0], inplace=True)
 df.category.fillna(df.category.mode()[0], inplace=True)
 df.age.fillna(int(df.age.mean()), inplace=True)
-df["annual income (lakhs)"].fillna(df["annual income (lakhs)"].mean(),
-                                   inplace=True)
+df["annual income (lakhs)"].fillna(df["annual income (lakhs)"].mean(), inplace=True)
 print(df.isnull().sum())
 
 # Data transformation
 
 encoder = preprocessing.LabelEncoder()
-df[["category", "purchase type ",
-    "gender"]] = df[["category", "purchase type ",
-                     "gender"]].apply(encoder.fit_transform)
+df[["category", "purchase type ", "gender"]] = df[
+    ["category", "purchase type ", "gender"]
+].apply(encoder.fit_transform)
 print(df.head())
 
 scaler = preprocessing.MinMaxScaler()
-df[["spending score", "items purchased (monthly)",
-    "annual income (lakhs)"]] = scaler.fit_transform(df[[
-        "spending score", "items purchased (monthly)", "annual income (lakhs)"
-    ]])
+df[
+    ["spending score", "items purchased (monthly)", "annual income (lakhs)"]
+] = scaler.fit_transform(
+    df[["spending score", "items purchased (monthly)", "annual income (lakhs)"]]
+)
 print(df.head())
 
 features = df[["age", "gender", "annual income (lakhs)"]]
 target = df["purchase type "]
 
 # This is not required in this, as we are doing clustering
-x_train, x_test, y_train, y_test = train_test_split(features,
-                                                    target,
-                                                    random_state=0,
-                                                    test_size=0.3)
+x_train, x_test, y_train, y_test = train_test_split(
+    features, target, random_state=0, test_size=0.3
+)
 
 from sklearn.cluster import KMeans
 
 
-#Add income too, it is a good parameter to form clusters (Not necessarily)
-dataframe = df[['gender', 'age', 'category']]
+# Add income too, it is a good parameter to form clusters (Not necessarily)
+dataframe = df[["gender", "age", "category"]]
 
 
 kmeans = KMeans(n_clusters=5, random_state=0)
@@ -53,8 +52,8 @@ kmeans.fit_predict(dataframe)
 print("Cluster centers\n", kmeans.cluster_centers_)
 dataframe["cluster"] = kmeans.labels_
 
-#A better reprsentation of clusters would be this.
-sns.scatterplot(data=dataframe,x = dataframe.age,y = dataframe.category,hue='cluster')
+# A better reprsentation of clusters would be this.
+sns.scatterplot(data=dataframe, x=dataframe.age, y=dataframe.category, hue="cluster")
 plt.show()
 
 sns.scatterplot(data=dataframe, x=dataframe.cluster, y=dataframe.age)
@@ -63,10 +62,27 @@ plt.show()
 sns.scatterplot(
     data=dataframe,
     x=dataframe.cluster,
-    y=dataframe.gender.replace({
-        0: "Male",
-        1: "Female"
-    }),
+    y=dataframe.gender.replace({0: "Male", 1: "Female"}),
 )
+plt.show()
+
+
+from sklearn.cluster import AgglomerativeClustering
+
+agg = AgglomerativeClustering(n_clusters=5, affinity="euclidean")
+agg.fit_predict(dataframe)
+dataframe["cluster"] = agg.labels_
+
+sns.scatterplot(data=dataframe, x=dataframe.cluster, y=dataframe.age)
+plt.show()
+
+sns.scatterplot(
+    data=dataframe,
+    x=dataframe.cluster,
+    y=dataframe.gender.replace({0: "Male", 1: "Female"}),
+)
+plt.show()
+
+sns.scatterplot(data=dataframe, x=dataframe.cluster, y=dataframe.category)
 plt.show()
 
